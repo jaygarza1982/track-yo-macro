2e478cb85f49a2e015f6f6bb68604487b1990bea
Jake Garza <jaygarza1982@gmail.com>
2021-12-21 22:05:48 -0500

    Dockerized server, added food model

---
+++ server/models/food.go
@@ -0,0 +1,12 @@
+package models
+
+type Food struct {
+	Name        string
+	Type        string
+	Quantity    float32
+	Calories    float32
+	Protein     float32
+	Carbs       float32
+	Fat         float32
+	Description string
+}
