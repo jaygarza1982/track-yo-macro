04a40e2b903a345ad81367a3c766780c60b6aa24
BuildTools <unconfigured@null.spigotmc.org>
2021-12-26 19:14:49 -0500

    Modified food models for consumed to have quantity

--- server/models/consumed.go
+++ server/models/consumed.go
@@ -5,5 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Consumed struct {
 	ID                primitive.ObjectID `bson:"_id" json:"_id"`
 	FoodID            primitive.ObjectID `bson:"foodId" json:"foodId"`
+	Quantity          float32            `json:"quantity,omitempty" bson:"quantity,omitempty"`
 	TimeConsumedEpoch int64              `json:"timeConsumedEpoch,omitempty" bson:"timeConsumedEpoch,omitempty"`
 }
--- server/models/food.go
+++ server/models/food.go
@@ -6,7 +6,6 @@ type Food struct {
 	ID          primitive.ObjectID `bson:"_id" json:"_id"`
 	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
 	Type        string             `json:"type,omitempty" bson:"type,omitempty"`
-	Quantity    float32            `json:"quantity,omitempty" bson:"quantity,omitempty"`
 	Calories    float32            `json:"calories,omitempty" bson:"calories,omitempty"`
 	Protein     float32            `json:"protein,omitempty" bson:"protein,omitempty"`
 	Carbs       float32            `json:"carbs,omitempty" bson:"carbs,omitempty"`
