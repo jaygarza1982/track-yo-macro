f8acfb33ec98f89ca597448c14e7dd0e9f7061d2
Jake Garza <jaygarza1982@gmail.com>
2022-01-05 19:01:05 -0500

    Food search at top of home

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AddFood from '../Food/AddFood';
 import FoodList from '../Food/FoodList';
 import useFetch from '../Hooks/useFetch';
+import FoodSearch from '../Search/FoodSearch';
 
 const Home = () => {
 
     const [foods, failedFoods, loadFoods] = useFetch('/api/food/list');
+    const [shownFoods, setShownFoods] = useState([]);
+
+    // Show all foods on load
+    useEffect(() => {
+        setShownFoods(foods);
+    }, [foods]);
 
     return (
         <div className='home'>
-            { !failedFoods && <FoodList foods={foods} /> }
+            <FoodSearch foods={foods} setShownFoods={setShownFoods} />
+            { !failedFoods && <FoodList foods={shownFoods} /> }
             <AddFood reloadFoods={loadFoods} />
         </div>
     )
---
+++ web/src/components/Search/FoodSearch.jsx
@@ -0,0 +1,45 @@
+import { TextField } from '@mui/material';
+import React, { useState } from 'react';
+
+const FoodSearch = ({ foods, setShownFoods }) => {
+
+    const [search, setSearch] = useState('');
+
+    const handleSearchChange = e => {
+        const { value } = e.target;
+        setSearch(value);
+
+        // Show all foods if empty search
+        if (value?.trim() == '') {
+            setShownFoods(foods);
+            return;
+        }
+
+        const lowerValue = value?.toLowerCase();
+
+        let matchedFoods = [];
+
+        foods?.forEach(food => {
+            if (food?.name?.toLowerCase()?.includes(lowerValue)) {
+                matchedFoods.push(food);
+            }
+        });
+
+        setShownFoods(matchedFoods);
+    }
+
+    return (
+        <div className='food-search'>
+            <TextField
+                label={'Search'}
+                type={'text'}
+                value={search}
+                onChange={handleSearchChange}
+                autoComplete='off'
+                onFocus={e => e.target.select()}
+            />
+        </div>
+    );
+}
+
+export default FoodSearch;
